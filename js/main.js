7dc2b104324d33f2540d4b3d0c08e25da8f3fbe5
MathiasChirivi <chirivimathias3@gmail.com>
2023-05-12 11:22:00 +0200

    fix code

--- js/main.js
+++ js/main.js
@@ -109,7 +109,7 @@ createApp({
                 },
                 {
                     name: 'Claudia',
-                    avatar: './img/avatar_5.jpg',
+                    avatar: './img/avatar_6.jpg',
                     visible: true,
                     messages: [
                         {
